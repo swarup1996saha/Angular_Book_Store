3ae274338ece8d6baddf3e8fb4fc9980ab5abfd8
swarup1996saha <hoichoi1018@gmail.com>
2022-08-13 10:26:28 +0530

    shared components creation

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { CartItemComponent } from './cart-item/cart-item.component';
+import { PriceDetailComponent } from './price-detail/price-detail.component';
+import { SidePannelComponent } from './side-pannel/side-pannel.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    HomeComponent,
+    ProductComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    CartItemComponent,
+    PriceDetailComponent,
+    SidePannelComponent
+  ],
   imports: [
     CommonModule
   ]
