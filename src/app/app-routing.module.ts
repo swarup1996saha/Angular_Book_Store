2c722539421b23a0bef525a82cfcfff305ce7042
swarup1996saha <hoichoi1018@gmail.com>
2022-08-14 11:25:11 +0530

    empty cart styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './shared/cart/cart.component';
 import { HomeComponent } from './shared/home/home.component';
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
+  {path:'home', redirectTo:'', pathMatch:'full'},
+  {path:'cart', component:CartComponent},
 ];
 
 @NgModule({
